3be119ef14c93cb091b562cba8658d04df80fa9a
David Maduabuchi <davidmaduabuchi62@gmail.com>
2025-03-21 01:01:03 +0100

    Database Updates

--- main/db.js
+++ main/db.js
@@ -1,22 +1,22 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "fabex-db",
-  password: "cookies",
-  port: 5432,
-});
-
 // const pool = new Pool({
-//   user: "fabex_user",
-//   host: "dpg-cri03tbv2p9s73bhlm2g-a.oregon-postgres.render.com",
-//   database: "fabex",
-//   password: "1x4FE04fBLVaiF9XafApKiQGi5zMvZpM",
+//   user: "postgres",
+//   host: "localhost",
+//   database: "fabex-db",
+//   password: "cookies",
 //   port: 5432,
-//   ssl: {
-//     rejectUnauthorized: false, // Ensure secure connection
-//   },
 // });
+
+const pool = new Pool({
+  user: "fabex_db_user",
+  host: "dpg-cvea8tpu0jms73be2110-a.oregon-postgres.render.com",
+  database: "fabex_db",
+  password: "ZA3GpWV5d4SnQRpo0ljtz1EM0dVtJdUD",
+  port: 5432,
+  ssl: {
+    rejectUnauthorized: false, // Ensure secure connection
+  },
+});
 
 module.exports = pool;
